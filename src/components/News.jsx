105e0141e18120fb19c64bc89749daa7e7904835
Amaljith <amaljithaurify@gmail.com>
2026-03-06 10:51:29 +0530

    first commit

---
+++ src/components/News.jsx
@@ -0,0 +1,100 @@
+import React from "react";
+import { Box, Typography } from "@mui/material";
+
+const NewsTicker = ({ newsItems = [] }) => {
+  // Ensure enough items for smooth scrolling
+  const tickerItems =
+    newsItems.length <= 1 ? Array(5).fill(newsItems[0]) : newsItems;
+
+  return (
+    <Box
+      sx={{
+        width: "100%",
+        // height: "3vw",
+        height: {
+          xs: "35px",
+          lg: "3vw",
+        },
+        display: "flex",
+        alignItems: "center",
+        overflow: "hidden",
+        background: " rgba(0,0,0,.4)",
+
+
+      }}
+    >
+      {/* LEFT BRAND */}
+      <Typography
+        sx={{
+          color: "#FFFFFF",
+          background: '  #36002F',
+          // fontSize: "1.2vw",
+          fontSize: {
+            xs: "16px",
+            lg: "1.2vw",
+          },
+          fontWeight: '700',
+          whiteSpace: "nowrap",
+          padding: "0 3.5vw",
+          height: "100%",
+          display: "flex",
+          alignItems: "center",
+          borderRadius: '10px',
+          justifyContent: "center",
+
+          flexShrink: 0,
+        }}
+      >
+        NEW RAK GOLD
+      </Typography>
+
+      {/* SCROLL AREA */}
+      <Box sx={{ flex: 1, overflow: "hidden" }}>
+        <Box
+          sx={{
+            whiteSpace: "nowrap",
+            display: "inline-flex",
+            alignItems: "center",
+            animation: "ticker 70s linear infinite",
+          }}
+        >
+          {tickerItems.map((item, index) => (
+            <Typography
+              key={index}
+              component="span"
+              sx={{
+                color: "#e6e6e6",
+                // fontSize: "1.3vw",
+                fontSize: {
+                  xs: "16px",
+                  lg: "1.3vw",
+                },
+                fontWeight: 500,
+                whiteSpace: "nowrap",
+                marginRight: "4vw",
+              }}
+            >
+              {item?.description || ""}
+            </Typography>
+          ))}
+        </Box>
+      </Box>
+
+      {/* KEYFRAMES */}
+      <style>
+        {`
+          @keyframes ticker {
+            0% {
+              transform: translateX(30%);
+            }
+            100% {
+              transform: translateX(-100%);
+            }
+          }
+        `}
+      </style>
+    </Box>
+  );
+};
+
+export default NewsTicker;
---
+++ src/pages/tvscreenView.jsx
@@ -0,0 +1,260 @@
+import React, { useCallback, useEffect, useState } from "react";
+import { Grid, Paper, Typography, Box, useMediaQuery } from "@mui/material";
+import SpotRate from "../components/SpotRate";
+import CommodityTable from "../components/CommodityTable";
+import NewsTicker from "../components/News";
+import MainLogo from "/images/logo.svg";
+import AurifyLogo from "/images/logo.svg";
+import backgroundImage from "/images/background1.png";
+
+import {
+  fetchSpotRates,
+  fetchServerURL,
+  fetchNews,
+  fetchTVScreenData,
+} from "../api/api";
+import io from "socket.io-client";
+import { useSpotRate } from "../context/SpotRateContext";
+import WorldClock from "../components/WorldClock";
+
+import PoweredByAurify from "../components/PoweredByAurify";
+import SystemClock from "../components/SystemClock";
+
+function TvScreen() {
+  const [showLimitModal, setShowLimitModal] = useState(false);
+  const [dateTime, setDateTime] = useState(new Date());
+  const [serverURL, setServerURL] = useState("");
+  const [news, setNews] = useState([]);
+  const [marketData, setMarketData] = useState({});
+  const [commodities, setCommodities] = useState([]);
+  const [goldBidSpread, setGoldBidSpread] = useState("");
+  const [goldAskSpread, setGoldAskSpread] = useState("");
+  const [silverBidSpread, setSilverBidSpread] = useState("");
+  const [silverAskSpread, setSilverAskSpread] = useState("");
+  const [symbols, setSymbols] = useState(["GOLD", "SILVER"]);
+  const [error, setError] = useState(null);
+
+  const { updateMarketData } = useSpotRate();
+
+  const adminId = import.meta.env.VITE_APP_ADMIN_ID;
+
+  updateMarketData(
+    marketData,
+    goldBidSpread,
+    goldAskSpread,
+    silverBidSpread,
+    silverAskSpread,
+  );
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const [spotRatesRes, serverURLRes, newsRes] = await Promise.all([
+          fetchSpotRates(adminId),
+          fetchServerURL(),
+          fetchNews(adminId),
+        ]);
+
+        // Handle Spot Rates
+        const {
+          commodities,
+          goldBidSpread,
+          goldAskSpread,
+          silverBidSpread,
+          silverAskSpread,
+        } = spotRatesRes.data.info;
+        setCommodities(commodities);
+        setGoldBidSpread(goldBidSpread);
+        setGoldAskSpread(goldAskSpread);
+        setSilverBidSpread(silverBidSpread);
+        setSilverAskSpread(silverAskSpread);
+
+        // Handle Server URL
+        const { serverURL } = serverURLRes.data.info;
+        setServerURL(serverURL);
+
+        // Handle News
+        setNews(newsRes.data.news.news);
+
+        console.log(newsRes.data);
+      } catch (error) {
+        console.log("Error fetching data:", error);
+        setError("An error occurred while fetching data");
+      }
+    };
+
+    fetchData();
+
+    // Fetch TV screen data (you can leave this as a separate call)
+    fetchTVScreenData(adminId)
+      .then((response) => {
+        console.log(response);
+        if (response.status === 200) {
+          // Allow TV screen view
+          setShowLimitModal(false);
+        }
+      })
+      .catch((error) => {
+        if (error.response && error.response.status === 403) {
+          setShowLimitModal(true); // Show the modal on 403 status
+        } else {
+          console.error("Error:", error.message);
+          alert("An unexpected error occurred.");
+        }
+      });
+  }, [adminId]);
+
+  // Function to Fetch Market Data Using Socket
+  useEffect(() => {
+    if (serverURL) {
+      const socket = io(serverURL, {
+        query: { secret: import.meta.env.VITE_APP_SOCKET_SECRET_KEY },
+        transports: ["websocket"],
+        withCredentials: true,
+      });
+
+      socket.on("connect", () => {
+        console.log("Connected to WebSocket server");
+        socket.emit("request-data", symbols);
+      });
+
+      socket.on("disconnect", () => {
+        console.log("Disconnected from WebSocket server");
+      });
+
+      socket.on("market-data", (data) => {
+        if (data && data.symbol) {
+          setMarketData((prevData) => ({
+            ...prevData,
+            [data.symbol]: {
+              ...prevData[data.symbol],
+              ...data,
+            },
+          }));
+        } else {
+          console.warn("Received malformed market data:", data);
+        }
+      });
+
+      socket.on("error", (error) => {
+        console.error("WebSocket error:", error);
+        setError("An error occurred while receiving data");
+      });
+
+      // Cleanup function to disconnect the socket
+      return () => {
+        socket.disconnect();
+      };
+    }
+  }, [serverURL, symbols]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setDateTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <Box
+      sx={{
+        minHeight: "100vh",
+        color: "white",
+        pb: " 3vw ",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "black",
+      }}
+    >
+      <Box
+        sx={{
+          height: "100%",
+          width: "100%",
+
+          position: "absolute",
+          inset: 0,
+          zIndex: 0,
+          overflow: "hidden",
+        }}
+      >
+        <img src={backgroundImage} alt="" className="object-cover w-full" />
+      </Box>
+      {/* Grid */}
+      <Grid
+        container
+        spacing={4}
+        minHeight="100%"
+        // alignItems="flex-start"
+        justifyContent="space-between"
+        padding="1vw"
+        flexWrap="wrap"
+        zIndex="1"
+        position="relative"
+        margin="0"
+      >
+        {/* Side: Commodity Table */}
+        <Grid
+          item
+          xs={12}
+          md={12}
+          display="flex"
+          alignItems="center"
+          justifyContent="start"
+        >
+          <SpotRate />
+        </Grid>
+
+        {/* Side: SpotRate & Date Time */}
+        <Grid
+          item
+          md={12}
+          xs={12}
+          display="grid"
+          gridTemplateColumns="1fr 1fr"
+        >
+          <Box
+            display="flex"
+            alignItems="center"
+            flexDirection="column"
+            justifyContent="center"
+          >
+            <Box
+              sx={{
+                height: "auto",
+                width: {
+                  xs: "30vw",
+                  lg: "20vw",
+                },
+                marginBottom: "1vw",
+              }}
+            >
+              <img src={MainLogo} alt="" className="object-contain w-full" />
+            </Box>
+            {/* <SystemClock /> */}
+            <WorldClock />
+            <PoweredByAurify />
+          </Box>
+          <Box>
+            <CommodityTable commodities={commodities} />
+          </Box>
+        </Grid>
+
+        <Grid
+          md={12}
+          sx={{
+            position: "fixed",
+            bottom: "0",
+            width: "100%",
+            left: "0",
+          }}
+        >
+          <NewsTicker newsItems={news} />
+        </Grid>
+      </Grid>
+    </Box>
+  );
+}
+
+export default TvScreen;
