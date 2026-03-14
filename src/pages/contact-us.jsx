c5dab4bf72bb4fe6260c3bc4ce2619d983979f78
Amaljith <amaljithaurify@gmail.com>
2026-03-14 11:11:43 +0530

    changes

--- src/pages/contact-us.jsx
+++ src/pages/contact-us.jsx
@@ -263,7 +263,7 @@ export default function ContactUs() {
             >
               <Typography
                 sx={{
-                  fontSize: { xs: "2rem", md: "2.4rem" },
+                  fontSize: { xs: "20px", sm: "2.4rem", md: "2.8rem" },
                   letterSpacing: "0.05em",
                   color: C.text,
                   mb: 0.5,
@@ -351,7 +351,7 @@ export default function ContactUs() {
             >
               <Typography
                 sx={{
-                  fontSize: { xs: "2rem", md: "2.4rem" },
+                  fontSize: { xs: "20px", sm: "2.4rem", md: "2.8rem" },
                   letterSpacing: "0.05em",
                   color: C.text,
                   mb: 3,
